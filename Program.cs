8da0790e4b7fd9dd4ccb5a9cb8a659c37b161211
PikeCaptain <idunno6418@gmail.com>
2024-03-01 14:42:03 -0600

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,102 @@
+﻿namespace Operators_Landon_Simmons
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // intializes and integer that mutiplies two integers
+            int intMult = 7 * 34;
+            //Intialized and integer that adds to integers
+            int intAdd = 55 + 77;
+            //Prints the maximum and minimum of the two integer variables
+            Console.WriteLine($"The max of {intMult} and {intAdd} is {Math.Max(intMult, intAdd)}. The min of the two is {Math.Min(intMult, intAdd)}");
+          
+
+            // intializes boolean to see if intMult is greater than intAdd
+            bool isIntGreater = intMult > intAdd;
+            // Prints id intMult is greter than intAdd
+            Console.WriteLine($"{intMult} is greater than {intAdd}, {isIntGreater}.");
+
+
+
+
+
+            // Intializes and shorteger that mutiplies two shortegers
+            short shortMult = 7 * 34;
+            //shortialized and shorteger that adds to shortegers
+            short shortAdd = 55 + 77;
+            //Prshorts the maximum and minimum of the two shorteger variables
+            Console.WriteLine($"The max of {shortMult} and {shortAdd} is {Math.Max(shortMult, shortAdd)}. The min of the two is {Math.Min(shortMult, shortAdd)}");
+           
+
+            // shortializes boolean to see if shortMult is greater than shortAdd
+            bool isshortGreater = shortMult > shortAdd;
+            // Prshorts id shortMult is greter than shortAdd
+            Console.WriteLine($"{shortMult} is greater than {shortAdd}, {isshortGreater}.");
+
+
+
+
+
+            // longializes and shorteger that mutiplies two shortegers
+            short longMult = 7 * 34;
+            //shortialized and shorteger that adds to shortegers
+            short longAdd = 55 + 77;
+            //Prshorts the maximum and minimum of the two shorteger variables
+            Console.WriteLine($"The max of {longMult} and {longAdd} is {Math.Max(longMult, longAdd)}. The min of the two is {Math.Min(longMult, longAdd)}");
+
+
+            // shortializes boolean to see if shortMult is greater than shortAdd
+            bool islongGreater = longMult > longAdd;
+            // Prshorts id shortMult is greter than shortAdd
+            Console.WriteLine($"{longMult} is greater than {longAdd}, {islongGreater}.");
+
+
+
+
+            // longializes and shorteger that mutiplies two shortegers
+            float floatMult = 7 * 34;
+            //floatialized and floateger that adds to floategers
+            float floatAdd = 55 + 77;
+            //Prfloats the maximum and minimum of the two floateger variables
+            Console.WriteLine($"The max of {floatMult} and {floatAdd} is {Math.Max(floatMult, floatAdd)}. The min of the two is {Math.Min(floatMult, floatAdd)}");
+
+
+            // floatializes boolean to see if floatMult is greater than floatAdd
+            bool isfloatGreater = floatMult > floatAdd;
+            // Prfloats id floatMult is greter than floatAdd
+            Console.WriteLine($"{floatMult} is greater than {floatAdd}, {isfloatGreater}.");
+
+
+
+
+            // longializes and shorteger that mutiplies two shortegers
+            double doubleMult = 7 * 34;
+            //shortialized and shorteger that adds to shortegers
+            double doubleAdd = 55 + 77;
+            //Prshorts the maximum and minimum of the two shorteger variables
+            Console.WriteLine($"The max of {doubleMult} and {doubleAdd} is {Math.Max(doubleMult, doubleAdd)}. The min of the two is {Math.Min(doubleMult, doubleAdd)}");
+
+
+            // shortializes boolean to see if shortMult is greater than shortAdd
+            bool isdoubleGreater = doubleMult > doubleAdd;
+            // Prshorts id shortMult is greter than shortAdd
+            Console.WriteLine($"{doubleMult} is greater than {doubleAdd}, {isdoubleGreater}.");
+
+
+
+            // longializes and shorteger that mutiplies two shortegers
+            decimal decimalMult = 7 * 34;
+            //shortialized and shorteger that adds to shortegers
+            decimal decimalAdd = 55 + 77;
+            //Prshorts the maximum and minimum of the two shorteger variables
+            Console.WriteLine($"The max of {decimalMult} and {decimalAdd} is {Math.Max(decimalMult, decimalAdd)}. The min of the two is {Math.Min(decimalMult, decimalAdd)}");
+
+
+            // shortializes boolean to see if shortMult is greater than shortAdd
+            bool isdecimalGreater = decimalMult > decimalAdd;
+            // Prshorts id shortMult is greter than shortAdd
+            Console.WriteLine($"{decimalMult} is greater than {decimalAdd}, {isdecimalGreater}.");
+        }
+    }
+}
